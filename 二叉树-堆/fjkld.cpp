6523b0f0c28b0d19d2f326a7f85a9c15dd521711
turn-sole <113235914+turn-sole@users.noreply.github.com>
2023-03-30 23:27:53 +0800

    Add files via upload

---
+++ 二叉树-堆/fjkld.cpp
@@ -0,0 +1,18 @@
+104. 二叉树的最大深度
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    int maxDepth(TreeNode* root) {
+        return root == nullptr ? 0 : 1 + max(maxDepth(root->right), maxDepth(root->left));
+    }
+};

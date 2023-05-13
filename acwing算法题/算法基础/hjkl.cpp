c13a533c4887cb7e2e61ea1ca905379701b83670
turn-sole <113235914+turn-sole@users.noreply.github.com>
2023-05-13 23:30:48 +0800

    Add files via upload

---
+++ acwing算法题/算法基础/hjkl.cpp
@@ -0,0 +1,42 @@
+//789. ÊýµÄ·¶Î§
+#include<iostream>
+using namespace std;
+
+const int y = 100010;
+int n, m, x, a[y];
+
+int main()
+{
+    scanf("%d%d", &n, &m);
+    for (int i = 0; i < n; i++)
+        scanf("%d", &a[i]);
+    while (m--)
+    {
+        scanf("%d", &x);
+        int l = 0, r = n - 1;
+        while (l < r)
+        {
+            int mid = l + r >> 1;
+            if (a[mid] < x)
+                l = mid + 1;
+            else
+                r = mid;
+        }
+        if (a[l] != x)
+        {
+            printf("-1 -1\n");
+            continue;
+        }
+        int l1 = l, r1 = n;
+        while (l1 + 1 < r1)
+        {
+            int mid = l1 + r1 >> 1;
+            if (a[mid] <= x)
+                l1 = mid;
+            else
+                r1 = mid;
+        }
+        printf("%d %d\n", l, l1);
+    }
+    return 0;
+}

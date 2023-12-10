e63a6a4a19aa46cef85a1665ca3a98c80ddd031d
turn-sole <113235914+turn-sole@users.noreply.github.com>
2023-12-10 23:49:02 +0800

    Add files via upload

---
+++ 面试经典150题/7t.cpp
@@ -0,0 +1,57 @@
+//45. Ã¯‘æ”Œœ∑ II
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        int ans = 0;
+        int end = 0;
+        int maxPos = 0;
+        for (int i = 0; i < nums.size() - 1; i++)
+        {
+            maxPos = max(nums[i] + i, maxPos);
+            if (i == end)
+            {
+                end = maxPos;
+                ans++;
+            }
+        }
+        return ans;
+    }
+};
+
+
+
+
+
+//274. H ÷∏ ˝
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        int n = citations.size();
+        vector<int> cnt(n + 1);
+        for (int c : citations) {
+            cnt[min(c, n)]++;
+        }
+        int s = 0;
+        for (int i = n; ; i--) {
+            s += cnt[i];
+            if (s >= i) {
+                return i;
+            }
+        }
+    }
+};
+
+
+class Solution {
+public:
+    int hIndex(vector<int>& citations) {
+        sort(citations.begin(), citations.end());
+        int h = 0, i = citations.size() - 1;
+        while (i >= 0 && citations[i] > h) {
+            h++;
+            i--;
+        }
+        return h;
+    }
+};
+
